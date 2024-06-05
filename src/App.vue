8bd7b2f66f8cd34c5b44bc1ea4aa8987133be08b
cesrra <c3z4r.rincon@gmail.com>
2024-06-05 08:41:13 -0500

    Add new funtionality and styles

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,8 @@ import TheWelcome from './components/TheWelcome.vue'
   
 // ref --> genera una variable reactiva
 const memes = ref([])
+const favorites = ref([])
+let allMemes = []
 
 const loadData = async () => {
   const respose = await fetch('https://api.imgflip.com/get_memes')
@@ -21,6 +23,28 @@ const loadData = async () => {
   console.log(data.memes)
   //Forma de asignar valores a variables reactivas | memes = data.memes --> daría error
   memes.value = data.memes
+  allMemes = data.memes
+}
+
+// Recibe el evento como parametro de la función
+const searchMeme = (event) => {
+  // Este event tiene la información del elemento que se está escuchando
+  console.log(event.target.value)
+  const { value } = event.target
+  // En el template no necesitas acceder al valuer, pero en la logica si
+  memes.value = allMemes.filter(meme => meme.name.toLowerCase().includes(value.toLowerCase()))
+}
+
+const handleFav = (memeId) => {
+  const index = favorites.value.indexOf(memeId)
+
+  if(index > -1) {
+    favorites.value.splice(index, 1)
+  }else {
+    favorites.value.push(memeId)
+  }
+
+  console.log(favorites.value)
 }
 
 // Si quisieramos ejecutar codigo cuando se crea el componente
@@ -78,14 +102,20 @@ todo lo que se valla a utilizar en el template
 <template>
   <header>
     <h1>Meme Search</h1>
-    <input class="base-input" type="text" placeholder="Buscar Meme" />
+
+    <!-- v-on:property_name = @property_name | On quiere decir que está escuchando ese evento -->
+    <div class="search">      
+      <input class="base-input" type="text" placeholder="Buscar Meme" @input="searchMeme" />
+    </div>
+
     <section class="container">
       <!-- v-for es una de las Directivas principales -->
       <!-- v-bind:property_name = :property_name | Bind quiere decir que el valor de esa propiedad es dinámica -->
       <div v-for="meme in memes" :key="meme.id" class="card" >
-        <!-- <HeartIcon class="icon" /> -->
         <p>{{ meme.name }}</p>
+        <!-- Aplicamos clases condicionales, eventos On de escucha -->
         <img :src="meme.url" :alt="meme.name">
+        <div class="icon" :class="{ selected: favorites.includes(meme.id) }" @click="handleFav(meme.id)" ></div>
       </div>
     </section>
   </header>
@@ -95,7 +125,7 @@ todo lo que se valla a utilizar en el template
   </main> -->
 </template>
 
-<!-- Si queiro que los estilos sólo se apliquen al componente usamos scoped como propiedad del style -->
+<!-- Si queiro que los estilos sólo se apliquen al componente usamos <style scoped> -->
 <style>
 @import './assets/base.css';
 
@@ -111,50 +141,65 @@ header {
   line-height: 1.5;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+header h1 {
+  text-align: center;
+  padding: 1%;
 }
 
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
+.search {
+  display: flex;
+  padding: 2.5%;
+  position: sticky;
+  top: 0;
+  z-index: 100;
+  background-color: white;
+  box-shadow: 0 2px 8px 0;
+  margin-bottom: 13px;
 }
 
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
+.base-input {
+  width: 100%;
 }
 
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.container {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-gap: 20px;
 }
+
+.icon {
+  width: 25px;
+  height: 25px;
+  margin: 5px;
+  background-color: white;
+  left: 75%;
+  border: solid 2px;
+  border-radius: 1rem;
+}
+
+.card {
+  max-width: 180px;
+  height: auto;
+  padding: 2%;
+  border: solid 2px;
+  border-radius: .5rem;
+}
+
+.card img {
+  width: 100%;
+  height: auto;
+  object-fit: cover;
+}
+
+.card p {
+  font-weight: bold;
+  text-align: center;
+  margin: 5px;
+}
+
+.selected {
+  background-color: blue;
+  border-color: #ff3b00a3;
+}
+
 </style>
