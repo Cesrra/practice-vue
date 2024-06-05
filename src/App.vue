089f08d3eae73897d9ea658bae3be0f22a16bd5e
cesrra <c3z4r.rincon@gmail.com>
2024-06-04 23:29:20 -0500

    Commented the mains files to understand

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,101 @@
+<!-- 
+- Se concoe como SFC(single file compoenent) 
+- Composition API en Vue 3 [script, template, style]
+- El único requerido es el template
+- Hay dos formas de escribir el script en Composition API
+-->
+
+<!-- Forma #1 -->
 <script setup>
+import { onMounted, ref } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+  
+// ref --> genera una variable reactiva
+const memes = ref([])
+
+const loadData = async () => {
+  const respose = await fetch('https://api.imgflip.com/get_memes')
+  const { data } = await respose.json()
+
+  console.log(data.memes)
+  //Forma de asignar valores a variables reactivas | memes = data.memes --> daría error
+  memes.value = data.memes
+}
+
+// Si quisieramos ejecutar codigo cuando se crea el componente
+loadData()
+
+// Si quisieramos ejecutar codigo cuando el componente esté montado
+onMounted(() => {
+  console.log('Montado')
+})
 </script>
+
+<!--  Forma #2
+<script>
+/*
+La diferencia principal es que en la forma #2 necesita retornar 
+todo lo que se valla a utilizar en el template
+*/
+  export default {
+    setup() {
+      const memes = ref([])
+      const loadData = async () => {
+        const respose = await fetch('https://api.imgflip.com/get_memes')
+        const { data } = await respose.json()
+
+        console.log(data.memes)
+        memes.value = data.memes
+      }
+
+      loadData()
+
+      return {
+        memes,
+      }
+    },
+  }
+</script>
+-->
+
+<!-- Option API en Vue 2 
+<script>
+  export default {
+    name: 'App',
+    mounted() {},
+    methods: {
+      async loadData() {
+        const respose = await fetch('https://api.imgflip.com/get_memes')
+        const { data } = await respose.json()
+        console.log(data.memes)
+      }
+    },
+  }
+</script>
+-->
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <h1>Meme Search</h1>
+    <input class="base-input" type="text" placeholder="Buscar Meme" />
+    <section class="container">
+      <!-- v-for es una de las Directivas principales -->
+      <!-- v-bind:property_name = :property_name | Bind quiere decir que el valor de esa propiedad es dinámica -->
+      <div v-for="meme in memes" :key="meme.id" class="card" >
+        <!-- <HeartIcon class="icon" /> -->
+        <p>{{ meme.name }}</p>
+        <img :src="meme.url" :alt="meme.name">
+      </div>
+    </section>
   </header>
 
-  <main>
+  <!-- <main>
     <TheWelcome />
-  </main>
+  </main> -->
 </template>
 
+<!-- Si queiro que los estilos sólo se apliquen al componente usamos scoped como propiedad del style -->
 <style>
 @import './assets/base.css';
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
+
+//Componente inciial de Vue
 import App from './App.vue'
 
+//Montamos el Compoenente incial en el punto de entrada (app)
 createApp(App).mount('#app')
